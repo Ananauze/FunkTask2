845dda726bde02d4213f2ca9a1279b9fbb4bc9f4
anananass <ananauze@gmail.com>
2023-05-22 18:22:52 +0400

    Add project files.

---
+++ FunkTask2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+
+namespace FunkTask2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            string symbolInBar = "#";
+            string emptySymbol = "_";
+            int percentsHealth = 75;
+            int maxHealth = 20;
+
+            Console.WriteLine("\nПривет! Давай нарисуем твой уровень здоровья!");
+            Console.WriteLine("\nДержи братишка, твое здоровье:");
+
+            DrawBar(percentsHealth, maxHealth, symbolInBar, emptySymbol);
+            
+            Console.ReadLine();
+        }
+
+        static void DrawBar(int percentHealth, int maxValue, string symbolInBar, string emptySymbol) 
+        {
+            string filledBar = "";
+            int percents = 100;
+            int value = maxValue * percentHealth / percents;
+
+            filledBar = FillBar(0, value, symbolInBar);
+             
+            Console.Write("[" + filledBar);
+
+            filledBar = FillBar(value, maxValue, emptySymbol);
+
+            Console.Write($"{filledBar}]");
+        }
+
+        static string FillBar(int i, int value, string symbol) 
+        {
+            string temporaryString = "";
+            
+            for (int j = i; j < value; j++)
+            {
+                temporaryString += symbol;
+            }
+
+            return temporaryString;
+        }
+    }
+}
